bcaf1cc20418d7af2708a6c0ce1e22d0b37569e0
Nabil Hadi <napilfadl@gmail.com>
2023-10-03 12:28:05 +0300

    Set up environment and database connection

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
-import express from "express";
+import app from "./app";
+import env from "./util/validateEnv";
+import mongoose from "mongoose";
 
-const app = express();
-const port = 5000;
+const port = env.PORT || 5000;
+const db_uri = env.MONGO_CONNECTION_STRING;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
-app.listen(port, () => {
-  console.log("Server listening on port " + port);
-});
+mongoose
+  .connect(db_uri)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log("Server listening on port " + port);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
