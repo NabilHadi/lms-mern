0323ce907d4ea9836125c3e7a19cd95fff8672b3
Nabil Hadi <napilfadl@gmail.com>
2023-10-04 10:47:15 +0300

    Test commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
-  let errorMessage = "An unknown error occurred";
+  let errorMessage = "An unknown error occurred.";
   if (error instanceof Error) {
     errorMessage = error.message;
   }
